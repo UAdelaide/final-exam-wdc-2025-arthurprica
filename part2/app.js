f1f02ce86afd59af0ea5a28d14c40db0d6f34db0
arthurprica <arthur.prica@student.adelaide.edu.au>
2025-06-20 08:55:42 +0000

    [automatic commit] 2025-06-20 08:55:42 UTC 122.199.19.174

--- part2/app.js
+++ part2/app.js
@@ -23,6 +23,7 @@ app.use(session({
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const dogRoutes = require('./')
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
