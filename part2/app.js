d09a8cb86c8c8f551da4e2270b38bf0cd9e1e746
arthurprica <arthur.prica@student.adelaide.edu.au>
2025-06-20 08:54:58 +0000

    [automatic commit] 2025-06-20 08:54:58 UTC 122.199.19.174

--- part2/app.js
+++ part2/app.js
@@ -26,6 +26,7 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/dogs', dog)
 
 // Export the app instead of listening here
 module.exports = app;
