d9af122683994c775c64dbe295eea1b4d1c586b7
arthurprica <arthur.prica@student.adelaide.edu.au>
2025-06-20 06:05:14 +0000

    [automatic commit] 2025-06-20 06:05:14 UTC 122.199.19.174

--- part2/app.js
+++ part2/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(session({
-  secret: process.env.SESSION_SECRET || 'dogsecret', // Secret for signing session ID cookies
+  secret: process.env.SESSION_SECRET || 'secret', // Secret for signing session ID cookies
   resave: false,                  // Don’t save session if unmodified
   saveUninitialized: false        // Don’t create session until something is stored
 }));
