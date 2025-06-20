2b62eff48058c21516c25e527d08c777011e2981
arthurprica <arthur.prica@student.adelaide.edu.au>
2025-06-20 09:07:38 +0000

    [automatic commit] 2025-06-20 09:07:38 UTC 122.199.19.174

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-router.get('/dogs', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const [rows] = await db.query(`
-      SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
+      SELECT   Dogs.dog_id, Dogs.name AS dog_name, Dogs.size, Dogs.owner_id,
+  Dogs.photo
       FROM Dogs
       JOIN Users ON Dogs.owner_id = Users.user_id
     `);
@@ -14,3 +15,5 @@ router.get('/dogs', async (req, res) => {
     res.status(500).json({ error: 'Failed to return list of dogs', details: err.message });
   }
 });
+
+module.exports = router;
