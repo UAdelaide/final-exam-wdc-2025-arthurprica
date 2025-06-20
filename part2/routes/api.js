a5a7cb4f9676c008ef94e93ccc0056a13c5f030a
arthurprica <arthur.prica@student.adelaide.edu.au>
2025-06-20 08:51:35 +0000

    [automatic commit] 2025-06-20 08:51:35 UTC 122.199.19.174

---
+++ part2/routes/api.js
@@ -0,0 +1,12 @@
+router.get('/dogs', async (req, res) => {
+  try {
+    const [rows] = await db.query(`
+      SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
+      FROM Dogs
+      JOIN Users ON Dogs.owner_id = Users.user_id
+    `);
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to return list of dogs', details: err.message });
+  }
+});
