192f004d9ae86d261cb49362fb5aa97bf074c772
arthurprica <arthur.prica@student.adelaide.edu.au>
2025-06-20 08:52:43 +0000

    [automatic commit] 2025-06-20 08:52:43 UTC 122.199.19.174

--- part2/routes/api.js
+++ part2/routes/api.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models/db');
+
 router.get('/dogs', async (req, res) => {
   try {
     const [rows] = await db.query(`
